daba3204110ff11169c1edf3206940fb36d746ba
Ariel-Hinca <hincapieariel@gmail.com>
2025-12-27 12:30:24 +0100

    GET-route voor imkers gefixt en correct geplaatst

--- routes/beekeepers.js
+++ routes/beekeepers.js
@@ -3,15 +3,17 @@ import { Beekeeper } from "../models/Beekeeper.js";
 
 const router = express.Router();
 
-// Nieuwe imker toevoegen
+// Nieuwe imker toevoegen aan DB
 router.post("/", async (req, res) => {
-  try {
     const beekeeper = new Beekeeper(req.body);
     const savedBeekeeper = await beekeeper.save();
-    res.status(201).json(savedBeekeeper);
-  } catch (error) {
-    res.status(400).json({ error: "Kon imker niet opslaan" });
-  }
+    res.json(savedBeekeeper);
+});
+
+// Alle imkers ophalen uit de DB
+router.get("/", async (req, res) => {
+    const beekeepers = await Beekeeper.find();
+    res.json(beekeepers);
 });
 
 export default router;
